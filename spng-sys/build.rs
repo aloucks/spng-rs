38120700473e82a19b9fa64ae063455ad615f437
Aaron Loucks <aloucks@cofront.net>
2020-03-13 20:15:08 -0400

    Set libz include only when available

--- spng-sys/build.rs
+++ spng-sys/build.rs
@@ -1,11 +1,12 @@
 use std::env;
 
 fn main() {
-    let libz_include = env::var_os("DEP_Z_INCLUDE").expect("DEP_Z_INCLUDE");
-    cc::Build::new()
-        .file("libspng/spng.c")
-        .include(libz_include)
-        .compile("spng");
+    let mut build = cc::Build::new();
+    build.file("libspng/spng.c");
+    if let Some(libz_include) = env::var_os("DEP_Z_INCLUDE") {
+        build.include(libz_include);
+    }
+    build.compile("spng");
 
     // DEP_SPNG_INCLUDE
     println!("include=libspng");
