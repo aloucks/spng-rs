64d518dddbf90e77016a7f3866aa7783a6913aec
Aaron Loucks <aloucks@cofront.net>
2021-08-28 10:17:56 -0400

    Update benchmarks to use png 0.17

--- spng-benchmarks/benches/png.rs
+++ spng-benchmarks/benches/png.rs
@@ -5,9 +5,9 @@ fn png_decode(c: &mut Criterion) {
         let mut buf = Vec::new();
         b.iter(|| {
             let d = png::Decoder::new(spng_benchmarks::TEST_PNG_002);
-            let (info, mut reader) = d.read_info().unwrap();
-            spng_benchmarks::reserve(&mut buf, info.buffer_size());
-            reader.next_frame(&mut buf).unwrap();
+            let mut reader = d.read_info().unwrap();
+            spng_benchmarks::reserve(&mut buf, reader.output_buffer_size());
+            let _info = reader.next_frame(&mut buf).unwrap();
             black_box(reader);
         })
     });
