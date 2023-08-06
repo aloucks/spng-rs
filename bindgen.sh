431cf749002012bf1fdbeee1d13d683b88ab8136
Aaron Loucks <aloucks@cofront.net>
2023-08-06 14:05:39 -0400

    Update bindgen and regenerate

--- bindgen.sh
+++ bindgen.sh
@@ -1,13 +1,12 @@
 VERSION=$(bindgen --version)
 bindgen -o spng-sys/src/ffi.rs \
  --raw-line "/* ${VERSION} */" \
- --whitelist-type "spng_.*" \
- --whitelist-var "SPNG_.*" \
- --whitelist-function "spng_.*" \
+ --allowlist-type "spng_.*" \
+ --allowlist-var "SPNG_.*" \
+ --allowlist-function "spng_.*" \
  --ctypes-prefix libc \
  --use-core \
  --impl-debug \
  --impl-partialeq \
- --size_t-is-usize \
  --opaque-type FILE \
  spng-sys/libspng/spng/spng.h
